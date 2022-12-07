b3bca24fdbd90a2221479e99312a271646ba051d
triplem123 <90787133+triplem123@users.noreply.github.com>
2022-12-06 19:01:11 -0500

    update pages

--- college_living/college_living/views.py
+++ college_living/college_living/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+<<<<<<< Updated upstream
 
 # home page of the application
 def home(request):
     return render(request, 'HomePage.html')
+=======
+from colleges.models import College
+
+
+# home page of the application
+def home(request):
+    college = College.approved_colleges.all()[:3]
+    return render(request, 'HomePage.html', {'colleges': college})
+>>>>>>> Stashed changes
