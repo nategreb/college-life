ab21446ee5ad5927074d08975bf74360aadfb270
Nate Grebelsky <nate.greb@gmail.com>
2022-12-07 17:01:21 -0500

    url changes, views + ui for professors

--- college_living/colleges/urls.py
+++ college_living/colleges/urls.py
@@ -1,17 +1,21 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'colleges'
 
 urlpatterns = [
-    path('<slug:college_name>/', views.college_home, name='college_home'),
-    path('<slug:college_name>/dorms', views.college_on_campus_living, name='dorms'),
-    path('<slug:college_name>/professors', views.college_home, name='professors'),
-    path('<slug:college_name>/classes', views.college_classes, name='classes'),
-    
-    
-    #moderation links
-    path('<slug:college_name>/edit/', views.edit_college),
-    path('<slug:college_name>/edit/residential-areas/', views.get_moderated_resAreas),
-    path('<slug:college_name>/edit/dorms', views.get_moderated_dorms)
+    path('<slug:college_name>/', include([
+        path('', views.college_home, name='college_home'),
+        path('dorms/', views.college_on_campus_living, name='dorms'),
+        path('classes/', views.college_classes, name='classes'),
+
+        # moderation links
+        path('edit/', views.edit_college),
+        path('edit/residential-areas/', views.get_moderated_resAreas),
+        path('edit/dorms', views.get_moderated_dorms),
+
+        # professors
+        path('professors', views.get_professors, name='professors'),
+        path('professors/<int:professor_id>/<slug:professor_slug>', views.get_professor, name='professor'),
+    ])),
 ]
