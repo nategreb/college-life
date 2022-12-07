a5d47d3f5e596ad9717615617f2b507a686c36c4
Nate Grebelsky <nate.greb@gmail.com>
2022-12-07 15:45:50 -0500

    changed Class model and added field to professors

--- college_living/colleges/models/professors.py
+++ college_living/colleges/models/professors.py
@@ -1,25 +1,48 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.template.defaultfilters import slugify
 
 from .colleges import College
-from .courses import CollegeClasses
+from .courses import CollegeClass
 from .departments import Department
 
- 
+
 class Professor(models.Model):
-    class Meta: 
-        constraints = [ 
-            models.UniqueConstraint(fields=['college', 'department', 'first_name', 'last_name'], 
-                                    name='class_subject_alt_key')        
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['college', 'department', 'first_name', 'last_name'],
+                                    name='class_subject_alt_key')
         ]
-        
-    college         =   models.ForeignKey(
-                            College,
-                            on_delete=models.CASCADE
-                        )        
-    department      =   models.ForeignKey(
-                            Department,
-                            on_delete=models.PROTECT
-                        )
-    first_name      =   models.CharField(max_length = 20)
-    last_name       =   models.CharField(max_length = 20)
-    classes         =   models.ManyToManyField(CollegeClasses)
+
+    college = models.ForeignKey(
+        College,
+        on_delete=models.CASCADE
+    )
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.PROTECT
+    )
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    classes = models.ManyToManyField(CollegeClass)
+
+    slug = models.SlugField(max_length=40, blank=True, null=False)
+
+    # add slug by converting white spaces to hyphens
+    def save(self, *args, **kwargs):
+        self.slug = slugify(f'{self.first_name} {self.last_name}')
+        # call full clean to validte
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    # validate the models fields
+    def clean(self):
+        if not self.first_name:
+            raise ValidationError({
+                'first_name': ('First Name can\'t be empty'),
+            })
+        if not self.last_name:
+            # field specific errors
+            raise ValidationError({
+                'last_name': ('Last Name can\'t be empty')
+            })
