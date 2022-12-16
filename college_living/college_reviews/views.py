b3351bc6be61ffa3457d3eb661df41134ec733fa
Vu Pham <levu2909@gmail.com>
2022-12-16 16:19:14 -0500

    add delete class review

--- college_living/college_reviews/views.py
+++ college_living/college_reviews/views.py
@@ -17,9 +17,11 @@ def create_course_review(request, college_name, course_id):
     if request.POST:
         form = AddCourseReviewForm(request.POST)
         if request.user.is_authenticated and form.is_valid():
+            # TODO: pop up show error message
             user = User.objects.get(pk=request.user.id)
             course = CollegeClass.objects.get(pk=course_id)
             if CourseReview.objects.filter(user=user, course=course).count() == 0:
+                # TODO: pop up show error message
                 CourseReview.objects.create(
                     course=course,
                     user=user,
@@ -30,7 +32,7 @@ def create_course_review(request, college_name, course_id):
                     take_again=form.cleaned_data['take_again'],
                     term=form.cleaned_data['term']
                 )
-        return redirect(list_course_review, college_name=college_name, course_id=course_id)
+        return redirect('reviews:class_review_home', college_name=college_name, course_id=course_id)
     return render(request, 'course_reviews/AddCourseReview.html', {'form': AddCourseReviewForm})
 
 
@@ -39,19 +41,31 @@ def list_course_review(request, college_name, course_id):
     reviews = CourseReview.objects.filter(course=course)
     content = {
         'reviews': reviews,
-        'course': course
+        'course': course,
+        'user': request.user.id,
+        'college': college_name
     }
     return render(request, 'course_reviews/CourseReviewHome.html', content)
 
 
 def edit_course_review(request, college_name, course_id, course_review_id):
-    review = CourseReview.objects.get(pk=course_review_id, course_id=course_id)
-    return render(request, 'course_reviews/AddCourseReview.html', {'form': AddCourseReviewForm()})
+    try:
+        review = CourseReview.objects.get(pk=course_review_id, course_id=course_id, user_id=request.user.id)
+        form = AddCourseReviewForm(instance=review)
+        if request.POST and request.user.is_authenticated and form.is_valid():
+            form.save()
+            return redirect(list_course_review, college_name=college_name, course_id=course_id)
+        return render(request, 'course_reviews/AddCourseReview.html', {'form': form})
+    except CourseReview.DoesNotExist:
+        # TODO: pop up show error message
+        return redirect('reviews:class_review_home', college_name=college_name, course_id=course_id)
 
 
 def delete_course_review(request, college_name, course_id, course_review_id):
-    review = CourseReview.objects.get(pk=course_review_id, course_id=course_id)
-    review.delete()
-    reviews = CourseReview.objects.filter(course_id=course_id)
-    return render(request, 'course_reviews/CourseReviewHome.html', {'reviews': reviews})
-
+    try:
+        review = CourseReview.objects.get(pk=course_review_id, course_id=course_id)
+        review.delete()
+    except CourseReview.DoesNotExist:
+        # TODO: pop up show error message
+        pass
+    return redirect('reviews:class_review_home', college_name=college_name, course_id=course_id)
