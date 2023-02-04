b81220e9e240a38a93aee226ddf3ab2d795f7082
Nate Grebelsky <nate.greb@gmail.com>
2023-02-04 14:24:53 -0500

    fixed search filter for professors

--- college_living/colleges/views/search_view.py
+++ college_living/colleges/views/search_view.py
@@ -1,5 +1,6 @@
 from django.contrib.postgres.search import SearchVector
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
 from django.urls import reverse_lazy
@@ -49,7 +50,7 @@ class ProfessorSearchView(ListView):
 
             objs = self.model.objects.annotate(
                 search=SearchVector('first_name', 'last_name')
-            ).filter(search__icontains=search_query)
+            ).filter(Q(search__icontains=search_query) | Q(search=search_query))
 
             if objs.count() == 1:
                 kwargs.update({'professor_id': objs[0].id, 'professor_slug': objs[0].slug})
