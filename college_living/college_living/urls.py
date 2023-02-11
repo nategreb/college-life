ea031318e7857d6da155cc10c639d8ed5f02488f
Nate Grebelsky <nate.greb@gmail.com>
2023-02-10 20:37:53 -0500

    improved the landing page

--- college_living/college_living/urls.py
+++ college_living/college_living/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', TemplateView.as_view(template_name='About.html'), name='about'),
 
     # third party
     path('accounts/', include('allauth.urls')),
