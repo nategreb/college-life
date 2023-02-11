894291b53178f4d326e3c442b165d23435146251
Nate Grebelsky <nate.greb@gmail.com>
2023-02-11 14:57:32 -0500

    added models to admin

--- college_living/colleges/admin.py
+++ college_living/colleges/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+"""Admin classes for the reviews app."""
+from django.contrib import admin
+
+from . import models
+
+
+class ProfessorAdmin(admin.ModelAdmin):
+    pass
+
+
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+
+class CollegeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.College, ProfessorAdmin)
+admin.site.register(models.Professor, CourseAdmin)
+admin.site.register(models.CollegeClass, CollegeAdmin)
